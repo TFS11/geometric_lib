450660529d6da400bbdddd3d6cf4db80143668a1
TFS11 <cakeivanov@mail.ru>
2023-10-04 23:17:36 +0300

    Adeed descriptions of functions

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    #Принимает значение радиуса круга, возвращает его площадь
     return math.pi * r * r
 
 
 def perimeter(r):
+    #Принимает значение радиуса круга, возвращает его периметр
     return 2 * math.pi * r
 
