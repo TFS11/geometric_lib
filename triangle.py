5fdd4e1053d5d2ca8a13f37282e04f29f44f168d
weity89 <govno_eto@mail.ru>
2023-10-05 01:55:54 +0300

    declaration of functions (triangle.py)

--- triangle.py
+++ triangle.py
@@ -1,5 +1,9 @@
-def area(a, h): 
+def area(a, h):
+    '''Принимает числа a и h,
+        возвращает значение половины от произведения a и h'''
     return a * h / 2 
 
-def perimeter(a, b, c): 
+def perimeter(a, b, c):
+    '''Принимает числа a, b и c,
+        возвращает сумму a, b и c'''
     return a + b + c 
